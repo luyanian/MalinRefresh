f994b5c7f8bfb6b6d304fafa1b7df4d62915c3f1
luyanian <luyanian@qq.com>
2017-02-22 17:15:39 +0800

    Initial commit

---
+++ malinrefresh/src/main/java/com/malim/refresh/adapter/RefreshListenerAdapter.java
@@ -0,0 +1,50 @@
+package com.malim.refresh.adapter;
+
+import com.malim.refresh.listener.RefreshListener;
+import com.malim.refresh.weight.MalinRefreshLayout;
+
+public abstract class RefreshListenerAdapter implements RefreshListener {
+        @Override
+        public void onPullingDown(MalinRefreshLayout refreshLayout, float fraction) {
+        }
+
+        @Override
+        public void onPullingUp(MalinRefreshLayout refreshLayout, float fraction) {
+        }
+
+        @Override
+        public void onPullDownReleasing(MalinRefreshLayout refreshLayout, float fraction) {
+        }
+
+        @Override
+        public void onPullUpReleasing(MalinRefreshLayout refreshLayout, float fraction) {
+        }
+
+        @Override
+        public void onRefresh(MalinRefreshLayout refreshLayout) {
+        }
+
+        @Override
+        public void onLoadMore(MalinRefreshLayout refreshLayout) {
+        }
+
+        @Override
+        public void onFinishRefresh() {
+
+        }
+
+        @Override
+        public void onFinishLoadMore() {
+
+        }
+
+        @Override
+        public void onRefreshCanceled() {
+
+        }
+
+        @Override
+        public void onLoadmoreCanceled() {
+
+        }
+}
